da4cf21b31a6a55499d35cd4dafc477ff8fce9cb
A sweshita <alapusweshitha@gmail.com>
2023-06-29 17:43:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -90,14 +90,14 @@ const currentProductSizes = document.querySelectorAll(".size");
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
     //change the current slide
-    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    wrapper.style.transform = `translateX(₹{-100 * index}vw)`;
 
     //change the choosen product
     choosenProduct = products[index];
 
     //change texts of currentProduct
     currentProductTitle.textContent = choosenProduct.title;
-    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductPrice.textContent = "₹" + choosenProduct.price;
     currentProductImg.src = choosenProduct.colors[0].img;
 
     //assing new colors
